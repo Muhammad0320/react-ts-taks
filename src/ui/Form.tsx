5517a0a0acadc97ca1ee68dad45b527badc3396f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-08 08:44:55 +0100

    chore: add update Form comp

--- src/ui/Form.tsx
+++ src/ui/Form.tsx
@@ -1,3 +1,35 @@
-const Form = () => {};
+import {
+  ComponentPropsWithRef,
+  EventHandler,
+  FormEvent,
+  forwardRef,
+  useRef,
+} from "react";
+
+type FormType = {
+  onSave: (vale: unknown) => void;
+} & ComponentPropsWithRef<"form">;
+
+type FormRef = {
+  clear: () => void;
+};
+
+const Form = forwardRef<FormRef, FormType>(
+  ({ children, onSave, ...otherProps }, ref) => {
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.currentTarget);
+
+      const data = Object.fromEntries(formData);
+
+      onSave(data);
+    };
+
+    return <form ref={formRef} onSubmit={handleSubmit} {...otherProps}></form>;
+  }
+);
 
 export default Form;
