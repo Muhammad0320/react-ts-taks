1c8a9132574a93408a057ba20ac02f96340ea510
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-08 10:06:45 +0100

    chore: completed the modal comp

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,3 +1,27 @@
-const Modal = () => {};
+import { useEffect, useRef } from "react";
+
+import { createPortal } from "react-dom";
+
+type ModalPropTypes = {
+  openModal: boolean;
+  closeModal: () => boolean;
+};
+
+const Modal = ({ openModal, closeModal }: ModalPropTypes) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (openModal) {
+      modalRef.current?.close();
+    } else {
+      modalRef.current?.showModal();
+    }
+  }, [openModal]);
+
+  return createPortal(
+    <dialog ref={modalRef} onCancel={closeModal}></dialog>,
+    document.querySelector("#modal-root")!
+  );
+};
 
 export default Modal;
