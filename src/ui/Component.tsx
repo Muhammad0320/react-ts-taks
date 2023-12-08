38bc46ed8e532eb5b1461f1f0666e821a2bbfeb9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-08 09:20:39 +0100

    chore: completed the component comp

--- src/ui/Component.tsx
+++ src/ui/Component.tsx
@@ -1,3 +1,22 @@
-const Component = () => {};
+import {
+  type ReactNode,
+  type ComponentPropsWithoutRef,
+  type ElementType,
+} from "react";
+
+type ComponentType<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+function Component<U extends ElementType>({
+  as,
+  children,
+  ...props
+}: ComponentType<U>) {
+  const Components = as || "div";
+
+  return <Components {...props}> {children} </Components>;
+}
 
 export default Component;
