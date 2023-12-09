da1774f73795cc180d453f6966724f6fc39c6262
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-09 06:43:28 +0100

    chore: add session data

--- src/pages/Session.tsx
+++ src/pages/Session.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-
 import { SESSIONS } from "../dummy-sessions.ts";
+import BookingForm from "../ui/BookingForm.tsx";
+import Button from "../ui/Button.tsx";
 
 export default function SessionPage() {
   const params = useParams<{ id: string }>();
 
   const sessionId = params.id;
   const loadedSession = SESSIONS.find((session) => session.id === sessionId);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+  };
 
   if (!loadedSession) {
     return (
@@ -18,6 +30,13 @@ export default function SessionPage() {
 
   return (
     <main id="session-page">
+      {modalIsOpen && (
+        <BookingForm
+          onCloseModal={handleCloseModal}
+          modalIsOpen={modalIsOpen}
+        />
+      )}
+
       <article>
         <header>
           <img src={loadedSession.image} alt={loadedSession.title} />
@@ -32,6 +51,11 @@ export default function SessionPage() {
             </time>
             <p>
               {/* Todo: Add button that opens "Book Session" dialog / modal */}
+
+              <Button el="button" onClick={handleOpenModal}>
+                {" "}
+                Book Now!{" "}
+              </Button>
             </p>
           </div>
         </header>
