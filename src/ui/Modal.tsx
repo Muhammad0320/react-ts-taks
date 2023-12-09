84fdf8b17f8e8e333df383bcc55613bc77eb04cb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-09 20:58:50 +0100

    chore: update thr modal comp

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,25 +1,26 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 import { createPortal } from "react-dom";
 
 type ModalPropTypes = {
   closeModal: () => void;
   children: ReactNode;
+  openModal: boolean;
 };
 
-const Modal = ({ closeModal, children }: ModalPropTypes) => {
-  // const modalRef = useRef<HTMLDialogElement>(null);
+const Modal = ({ closeModal, children, openModal }: ModalPropTypes) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
 
-  // useEffect(() => {
-  //   if (openModal) {
-  //     modalRef.current?.close();
-  //   } else {
-  //     modalRef.current?.showModal();
-  //   }
-  // }, [openModal]);
+  useEffect(() => {
+    if (openModal) {
+      modalRef.current?.close();
+    } else {
+      modalRef.current?.showModal();
+    }
+  }, [openModal]);
 
   return createPortal(
-    <dialog onCancel={closeModal} className="modal" open>
+    <dialog onCancel={closeModal} className="modal" open={openModal}>
       {children}
     </dialog>,
 
