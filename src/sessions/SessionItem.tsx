f8adf4587dd77c68f53da6d4fb908efa187143f6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-09 05:01:28 +0100

    chore: complete session comp

--- src/sessions/SessionItem.tsx
+++ src/sessions/SessionItem.tsx
@@ -1,7 +1,29 @@
 import { type SessionItem } from "../../store/BookingContext";
+import Button from "../ui/Button";
 
-const SessionItem = ({ item }: SessionItem) => {
-  const { summary, date, title } = item;
+const SessionItemContainer = ({ summary, title, date }: SessionItem) => {
+  return (
+    <article className="upcoming-session">
+      <div>
+        <h3> {title} </h3>
+        <p> {summary} </p>
+        <time dateTime={new Date(date).toISOString()}>
+          {new Date(date).toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}
+        </time>
+      </div>
+
+      <p className="actios">
+        <Button el="button" textOnly>
+          {" "}
+          cancel{" "}
+        </Button>
+      </p>
+    </article>
+  );
 };
 
-export default SessionItem;
+export default SessionItemContainer;
