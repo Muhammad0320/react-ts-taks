a9710e4a4dd36bd27738ca2fe9b70caf3a69f099
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-09 05:08:40 +0100

    chore: complete sessions modal

--- src/sessions/SessionsModal.tsx
+++ src/sessions/SessionsModal.tsx
@@ -1,3 +1,16 @@
-const SessionModal = () => {};
+import Button from "../ui/Button";
+import SessionList from "./SessionsList";
+
+const SessionModal = () => {
+  return (
+    <>
+      <h2> Upcoming session </h2>
+      <SessionList />
+      <p className="actions">
+        <Button el="button"> Close </Button>
+      </p>
+    </>
+  );
+};
 
 export default SessionModal;
